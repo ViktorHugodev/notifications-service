1d6eadc46a2b2c9a397d99d9c97ab57944386913
viktorhugodev <viktorhugo.dev@gmail.com>
2022-12-12 21:08:52 -0300

    add prisma and providers configs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { CreateNotificationBody } from './create-notification-body';
+import { PrismaService } from './prisma.service';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import {randomUUID as uuid} from 'node:crypto'
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prisma.notification.findMany()
+  }
+  @Post('')
+  async create(@Body() body: CreateNotificationBody) {
+   
+    const {content,category, recipientId} = body
+    return await this.prisma.notification.create({
+      data :{
+        id: uuid(),
+        content,
+        category,
+        recipientId
+      }
+    })
   }
 }
